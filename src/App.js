529e099ea54cac7cc266af54833d855e15bdfa1b
Justin Buth <jb15512@appliedmed.com>
2021-09-30 21:45:43 -0700

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import './App.css';
-<<<<<<< HEAD
 import Tile from './Tile';
 import Footer from './Footer';
 import MyNav from './MyNav';
 import {Row, Col} from 'react-bootstrap';
-=======
-import NavBar from './Navbar';
-
-import Tile from './Tile';
-import Footer from './Footer';
-import {Container,Col, Row} from 'react-bootstrap';
->>>>>>> 05eb176d9dbfbfb2442c05645f40f028c8a157fc
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <div>
       
       <MyNav />
 
         <main className="container-fluid">      
-          <div>
+          <div className="margin-top-row">
             <Row className="justify-content-sm-center bordered-red">
               <Col lg={6} className='bordered-yellow d-flex justify-content-center'>
                   <Tile title="App 1" />
@@ -43,31 +34,6 @@ function App() {
         <Footer/>
       
     </div>
-=======
-    
-      <div>
-        <NavBar/>
-        <Container>
-          
-          <Row className="d-flex justify-content-center">
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-          </Row>
-
-          <Row className="d-flex justify-content-center">
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-            <Col className="d-flex justify-content-center bordered"><Tile title='app1' /></Col>
-          </Row>
-        
-        </Container>
-        <Footer/>
-      </div>
-      
-
-    
->>>>>>> 05eb176d9dbfbfb2442c05645f40f028c8a157fc
   );
 }
 
